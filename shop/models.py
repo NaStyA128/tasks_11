f713df0415a097e8de49bcd9b9571336f8ffce1b
NaStyA128 <n.a.s.t.y.a28v@gmail.com>
2016-07-27 23:01:09 +0300

    Inital commit. New models.py in shop.

---
+++ shop/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+# from django.contrib.auth import get_user_model
+from django.conf import settings
+from django.utils.encoding import python_2_unicode_compatible
+
+# Create your models here.
+# User = get_user_model()
+
+
+class Categories(models.Model):
+    name = models.CharField(max_length=50)
+
+    @python_2_unicode_compatible
+    def __str__(self):
+        return self.name
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    category = models.ForeignKey(
+        Categories,
+        on_delete=models.CASCADE,
+    )
+    size = models.CharField(max_length=10)
+    colour = models.CharField(max_length=40)
+    price = models.FloatField()
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return '%s, %s, %s' % \
+               (self.name, self.size, self.colour)
+
+
+class Orders(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    products = models.ManyToManyField(Products, through='OrderProducts')
+    total_sum = models.FloatField()
+    date = models.DateTimeField(auto_now_add=True)
+
+
+class OrderProducts(models.Model):
+    order = models.ForeignKey(Orders)
+    product = models.ForeignKey(Products)
+    quantity = models.PositiveIntegerField()
+    sum = models.FloatField()
+
+
+class Tree(models.Model):
+    node = models.ForeignKey('self')
